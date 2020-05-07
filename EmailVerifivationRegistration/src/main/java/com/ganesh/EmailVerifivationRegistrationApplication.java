2a72f692176795c9d4d8c8836117c82c77ff058e
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-07 14:40:05 +0530

    importing Appication

---
+++ EmailVerifivationRegistration/src/main/java/com/ganesh/EmailVerifivationRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerifivationRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVerifivationRegistrationApplication.class, args);
+	}
+
+}
